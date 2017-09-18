a23c1ba97c7bb3f60ae5d493433ae07a43c0898e
Pattarapont <pattarapont96@gmail.com>
2017-09-18 21:44:56 +0700

    add form input data for perdict location

---
+++ form_input_location.php
@@ -0,0 +1,90 @@
+<div class="container">
+	<h1>กรุณากรอกข้อมูลการท่องเที่ยว</h1>
+	<div class="row">
+		<div class="col">
+			<div class="card">
+				<div class="card-header">
+					ข้อมูลส่วนตัว
+				</div>
+				<div class="card-body ">
+					<form class="was-validated">
+						<p>เพศ</p>
+						<div class="custom-controls-stacked d-block my-4">
+							<label class="custom-control custom-radio">
+								<input id="radioStacked1" name="radio-stacked" type="radio" class="custom-control-input" required>
+								<span class="custom-control-indicator"></span>
+								<span class="custom-control-description">ชาย</span>
+							</label>
+							<label class="custom-control custom-radio">
+								<input id="radioStacked2" name="radio-stacked" type="radio" class="custom-control-input" required>
+								<span class="custom-control-indicator"></span>
+								<span class="custom-control-description">หญิง</span>
+							</label>
+						</div>
+
+						<p>อายุ</p>
+						<select class="custom-select d-block col" required>
+							<option value="">โปรดเลือกอายุ</option>
+							<option value="1">50 - 60 ปี</option>
+							<option value="2">60 - 70 ปี</option>
+							<option value="3">มากกว่า 70 ปี</option>
+						</select>
+
+
+						<p>ภูมิลำเนา</p>
+						<select class="custom-select d-block col" required>
+							<option value="">ภูมิลำเนาของท่าน</option>
+							<option value="1">50 - 60 ปี</option>
+							<option value="2">60 - 70 ปี</option>
+							<option value="3">มากกว่า 70 ปี</option>
+						</select>
+
+						<p>อาชีพก่อนเกษียณ</p>
+						<select class="custom-select d-block col" required>
+							<option value="">โปรดเลือกอาชีพ</option>
+							<option value="1">50 - 60 ปี</option>
+							<option value="2">60 - 70 ปี</option>
+							<option value="3">มากกว่า 70 ปี</option>
+						</select>
+
+						<p>โรคประจำตัว</p>
+						<div class="custom-controls-stacked d-block my-4">
+							<label class="custom-control custom-radio">
+								<input id="radioStacked1" name="radio-stacked" type="radio" class="custom-control-input" required>
+								<span class="custom-control-indicator"></span>
+								<span class="custom-control-description">ไม่มี</span>
+							</label>
+							<label class="custom-control custom-radio">
+								<input id="radioStacked2" name="radio-stacked" type="radio" class="custom-control-input" required>
+								<span class="custom-control-indicator"></span>
+								<span class="custom-control-description">มี</span>
+							</label>
+						</div>
+
+
+						<select class="custom-select d-block col" required>
+							<option value="">โปรดเลือกโรคประจำตัว</option>
+							<option value="1">โรคหัวใจ</option>
+							<option value="2">เบําหวําน</option>
+							<option value="3">ควํามดันโลหิต</option>
+							<option value="1">ข้อเข่ําเสื่อม</option>
+							<option value="2">จอประสําทตําเสื่อม</option>
+							<option value="3">โรคอื่นๆ</option>
+
+						</select>
+
+						<p>การเคลื่อนไหวร่างกาย</p>
+						<select class="custom-select d-block col" required>
+							<option value="">โปรดเลือกการเคลื่อนไหวร่างกาย</option>
+							<option value="1">50 - 60 ปี</option>
+							<option value="2">60 - 70 ปี</option>
+							<option value="3">มากกว่า 70 ปี</option>
+						</select>
+
+					</form>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ form_input_personal.php
@@ -0,0 +1,106 @@
+<div class="container">
+	<h1>กรุณากรอกข้อมูลของท่าน</h1>
+	<div class="row">
+		<div class="col">
+			<div class="card">
+				<div class="card-header">
+					ข้อมูลส่วนตัว
+				</div>
+				<div class="card-body ">
+					<form class="was-validated">
+						
+						<div class="custom-controls-stacked d-block my-4">
+							<p>เพศ</p>
+							<label class="custom-control custom-radio">
+								<input id="radioStacked1" name="radio-stacked" type="radio" class="custom-control-input" required>
+								<span class="custom-control-indicator"></span>
+								<span class="custom-control-description">ชาย</span>
+							</label>
+							<label class="custom-control custom-radio">
+								<input id="radioStacked2" name="radio-stacked" type="radio" class="custom-control-input" required>
+								<span class="custom-control-indicator"></span>
+								<span class="custom-control-description">หญิง</span>
+							</label>
+						</div>
+
+						<div class="custom-controls-stacked d-block my-4">
+							<p>อายุ</p>
+							<select class="custom-select d-block col" required>
+								<option value="">โปรดเลือกอายุ</option>
+								<option value="1">50 - 60 ปี</option>
+								<option value="2">60 - 70 ปี</option>
+								<option value="3">มากกว่า 70 ปี</option>
+							</select>
+						</div>
+
+						<div class="custom-controls-stacked d-block my-4">
+							<p>ภูมิลำเนา</p>
+							<select class="custom-select d-block col" required>
+								<option value="">ภูมิลำเนาของท่าน</option>
+								<option value="1">50 - 60 ปี</option>
+								<option value="2">60 - 70 ปี</option>
+								<option value="3">มากกว่า 70 ปี</option>
+							</select>
+						</div>
+
+
+						<div class="custom-controls-stacked d-block my-4">
+							<p>อาชีพก่อนเกษียณ</p>
+							<select class="custom-select d-block col" required>
+								<option value="">โปรดเลือกอาชีพ</option>
+								<option value="1">รับราชการ</option>
+								<option value="2">ธุรกิจส่วนตัว</option>
+								<option value="3">ค้าขาย</option>
+								<option value="1">พนักงานบริษัท</option>
+								<option value="2">เกษตรกรรม</option>
+								<option value="3">รับจ้างทั่วไป</option>
+								<option value="3">อื่นๆ</option>
+							</select>
+						</div>
+
+
+						<div class="custom-controls-stacked d-block my-4">
+							<p>โรคประจำตัว</p>
+							<label class="custom-control custom-radio">
+								<input id="radioStacked1" name="radio-stacked" type="radio" class="custom-control-input" required>
+								<span class="custom-control-indicator"></span>
+								<span class="custom-control-description">ไม่มี</span>
+							</label>
+							<label class="custom-control custom-radio">
+								<input id="radioStacked2" name="radio-stacked" type="radio" class="custom-control-input" required>
+								<span class="custom-control-indicator"></span>
+								<span class="custom-control-description">มี</span>
+							</label>
+						</div>
+
+						
+						<select class="custom-select d-block col" required>
+							<option value="">โปรดเลือกโรคประจำตัว</option>
+							<option value="1">โรคหัวใจ</option>
+							<option value="2">เบาหวาน</option>
+							<option value="3">ความดันโลหิต</option>
+							<option value="4">ข้อเข่าเสื่อม</option>
+							<option value="5">จอประสาทตาเสื่อม</option>
+							<option value="6">โรคอื่นๆ</option>
+							
+						</select>
+
+						<div class="custom-controls-stacked d-block my-4">
+							<p>การเคลื่อนไหวร่างกาย</p>
+							<select class="custom-select d-block col" required>
+								<option value="">โปรดเลือกการเคลื่อนไหวร่างกาย</option>
+								<option value="1">เดินได้ปกติ</option>
+								<option value="2">เดินได้เล็กน้อย</option>
+								<option value="3">เดินได้เล็กน้อย/ใช้ไม้เท้า</option>
+								<option value="1">เดินได้เล็กน้อย/ใช้รถเข็น</option>
+								<option value="2">เดินไม่ได้/ใช้รถเข็น</option>
+							</select>
+						</div>
+
+					</form>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
