99a320effd10cde80021181a666932c1513eb8cf
Pattarapont <pattarapont96@gmail.com>
2017-09-26 01:52:32 +0700

    form register

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>ThaiCreate.Com Tutorials</title>
+<title></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>
 <body>
 <form name="form1" method="post" action="save_register.php">
@@ -8,29 +8,29 @@
   <table width="400" border="1" style="width: 400px">
     <tbody>
       <tr>
-        <td width="125"> &nbsp;Username</td>
+        <td width="125"> Username</td>
         <td width="180">
           <input name="txtUsername" type="text" id="txtUsername" size="20">
         </td>
       </tr>
       <tr>
-        <td> &nbsp;Password</td>
+        <td> Password</td>
         <td><input name="txtPassword" type="password" id="txtPassword">
         </td>
       </tr>
       <tr>
-        <td> &nbsp;Confirm Password</td>
+        <td> Confirm Password</td>
         <td><input name="txtConPassword" type="password" id="txtConPassword">
         </td>
       </tr>
       <tr>
-        <td>&nbsp;Name</td>
+        <td>Name</td>
         <td><input name="txtName" type="text" id="txtName" size="35"></td>
       </tr>
       <tr>
       </tr>
       <tr>
-        <td>&nbsp;Email</td>
+        <td>Email</td>
         <td><input name="txtEmail" type="text" id="txtEmail" size="35"></td>
       </tr>
       <tr>
@@ -38,7 +38,7 @@
     </tbody>
   </table>
   <br>
-  <input type="submit" name="Submit" value="Save">
+  <input type="submit" name="Submit" value="สมัครสมาชิก">
 </form>
 </body>
 </html>
---
+++ sign_in.php
@@ -0,0 +1,8 @@
+<?php include './include/include_head.php'; ?>
+<!DOCTYPE html>
+<html>
+<body>
+	<?php include 'nav_bar.php'; ?>
+	
+</body>
+</html>
