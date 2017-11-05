bc60de2fbcaf39e6d56052b1c4292ddbf275f2ee
pattarapon <pattarapont96@gmail.com>
2017-11-06 02:55:59 +0700

    fix var

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,13 +1,19 @@
 <?php
-require 'db_conn.php';
-?>
-<?php
+$db_server = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_dbname = "et_cbr";
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 function connect_db($db_server, $db_username, $db_password, $db_dbname) {
 	$conn = new mysqli($db_server, $db_username, $db_password, $db_dbname);
 
 	// Check connection
 	if ($conn->connect_error) {
-		echo "Failed to connect to MySQL: ".$conn->connect_error;
+		echo "Failed to connect to MySQL: " . $conn->connect_error;
 		exit;
 	} else {
 		// echo "Success: connected to MySQL Najaaaaaa <br>", PHP_EOL;
