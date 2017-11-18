c00cdfd59e130932fe20d2de6c6d7107e5fac953
pattarapon <pattarapont96@gmail.com>
2017-11-19 00:45:54 +0700

    set for new query

--- home.php
+++ home.php
@@ -6,5 +6,13 @@
 </head>
 <body>
 <?php include 'navbar.php';?>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus voluptatum pariatur, temporibus. Quisquam quidem blanditiis, ipsa cum temporibus repellat officia suscipit sapiente qui quod excepturi odit molestias sint non recusandae.</p></div>
+		</div>
+	</div>
+</section>
 </body>
 </html>
