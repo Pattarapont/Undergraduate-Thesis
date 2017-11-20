dd4bc674a8da2cc201061824b38414c9d1870b85
pattarapon <pattarapont96@gmail.com>
2017-11-21 03:07:13 +0700

    modified value

--- account.php
+++ account.php
@@ -6,7 +6,6 @@ include './include/include_head.php';
 include 'navbar.php';
 ?>
 	<link href="./jquery.Thailand.js/dist/jquery.Thailand.min.css" rel="stylesheet">
-
 	<title></title>
 </head>
 <body>
@@ -19,112 +18,91 @@ include 'navbar.php';
 					ข้อมูลส่วนตัว
 				</div>
 				<div class="card-body">
-					<form action="./include/getvariable.php" class="was-validated" id="inputpersonal" method="POST" name="account">
-						<div class="form-group">
-							<div class="row">
-								<div class="col-md-4 mb-4">
-									<p>เพศ</p>
-									<label class="custom-control custom-radio">
-										<input class="custom-control-input" id="gender" name="gender" required="" type="radio" value="1">
-										<span class="custom-control-indicator"></span>
-										<span class="custom-control-description">ชาย</span>
-									</label>
-									<label class="custom-control custom-radio">
-										<input class="custom-control-input" id="gender" name="gender" required="" type="radio" value="0">
-										<span class="custom-control-indicator"></span>
-										<span class="custom-control-description">หญิง</span>
-									</label>
-								</div>
-								<div class="col-md-4 mb-4">
-									<label for="validationDefault02">อายุ</label>
-									<select class="custom-select d-block col" id="age" name="age" required="">
-										<option disabled hidden="" selected value="">
-											-- อายุ --
-										</option>
-										<option value="1">
-											50 - 60 ปี
-										</option>
-										<option value="2">
-											60 - 70 ปี
-										</option>
-										<option value="3">
-											มากกว่า 70 ปี
-										</option>
-									</select>
-								</div>
-								<div class="col-md-4 mb-4">
-									<label for="validationDefault02">อาชีพ</label>
-									<select class="custom-select d-block col" id="career" name="career" required="">
-										<option disabled hidden="" selected value="">
-											-- โปรดเลือกอาชีพ --
-										</option>
-										<option value="1">
-											รับราชการ
-										</option>
-										<option value="2">
-											ธุรกิจส่วนตัว
-										</option>
-										<option value="3">
-											ค้าขาย
-										</option>
-										<option value="4">
-											พนักงานบริษัท
-										</option>
-										<option value="5">
-											เกษตรกรรม
-										</option>
-										<option value="6">
-											รับจ้างทั่วไป
-										</option>
-										<option value="7">
-											อื่นๆ
-										</option>
-									</select>
-								</div>
+					<form action="./include/getvariable.php" class="was-validated" id="inputpersonal" method="post" name="account">
+						<div class="form-row">
+							<div class="form-group col-md-4">
+								<p>เพศ</p><label class="custom-control custom-radio"><input class="custom-control-input" id="gender" name="gender" required="" type="radio" value="ชาย"> <span class="custom-control-indicator"></span> <span class="custom-control-description">ชาย</span></label> <label class="custom-control custom-radio"><input class="custom-control-input" id="gender" name="gender" required="" type="radio" value="หญิง"> <span class="custom-control-indicator"></span> <span class="custom-control-description">หญิง</span></label>
 							</div>
-							<div class="row">
-								<div class="col-md-6 mb-6">
-									<label for="validationDefault01">ชื่อ</label>
-									<input class="form-control" id="validationDefault01" name="firstname" placeholder="กรุณากรอกชื่อ" required="" type="text">
-								</div>
-								<div class="col-md-6 mb-6">
-									<label for="validationDefault01">นามสกุล</label>
-									<input class="form-control" id="validationDefault01" name="lastname" placeholder="กรุณากรอกนามสกุล" required="" type="text">
-								</div>
+							<div class="form-group col-md-4">
+								<label for="validationDefault02">อายุ</label> <select class="custom-select d-block col" id="age" name="age" required="">
+									<option disabled hidden="" selected value="">
+										-- อายุ --
+									</option>
+									<option value="50 - 60 ปี">
+										50 - 60 ปี
+									</option>
+									<option value="60 - 70 ปี">
+										60 - 70 ปี
+									</option>
+									<option value="มากกว่า 70 ปี">
+										มากกว่า 70 ปี
+									</option>
+								</select>
 							</div>
-							<div class="row">
-								<div class="col-md-6 mb-6">
-									<label for="validationDefault01">E-mail</label>
-									<input class="form-control" id="validationDefault01" placeholder="กรุณากรอก E-mail" required="" type="e-mail">
-								</div>
-								<div class="col-md-6 mb-6">
-									<label for="validationDefault02">เบอร์โทรศัพท์ (Username)</label>
-									<input class="form-control" id="validationDefault02" name="tel" placeholder="xxx-xxxxxxx" required="" type="text">
-								</div>
+							<div class="form-group col-md-4">
+								<label for="validationDefault02">อาชีพ</label> <select class="custom-select d-block col" id="career" name="career" required="">
+									<option disabled hidden="" selected value="">
+										-- โปรดเลือกอาชีพ --
+									</option>
+									<option value="รับราชการ">
+										รับราชการ
+									</option>
+									<option value="ธุรกิจส่วนตัว">
+										ธุรกิจส่วนตัว
+									</option>
+									<option value="ค้าขาย">
+										ค้าขาย
+									</option>
+									<option value="พนักงานบริษัท">
+										พนักงานบริษัท
+									</option>
+									<option value="เกษตรกรรม">
+										เกษตรกรรม
+									</option>
+									<option value="รับจ้างทั่วไป">
+										รับจ้างทั่วไป
+									</option>
+									<option value="อื่นๆ">
+										อื่นๆ
+									</option>
+								</select>
 							</div>
 						</div>
-						<div class="form-group" id="autoprovince">
-							<div class="row">
-								<div class="col-md-6 mb-6">
-									<label for="validationDefault04">ตำบล</label>
-									<input class="form-control" id="validationDefault04" name="district" placeholder="กรุณากรอกตำบล" required="" type="text">
-								</div>
-								<div class="col-md-6 mb-6">
-									<label for="validationDefault04">อำเภอ</label>
-									<input class="form-control" id="validationDefault04" name="amphoe" placeholder="กรุณากรอกอำเภอ" required="" type="text">
-								</div>
+						<div class="form-row">
+							<div class="form-group col-md-6">
+								<label for="validationDefault01">ชื่อ</label> <input class="form-control" id="validationDefault01" name="firstname" placeholder="กรุณากรอกชื่อ" required="" type="text">
 							</div>
-							<div class="row">
-								<div class="col-md-6 mb-6">
-									<label for="validationDefault04">จังหวัด</label>
-									<input class="form-control" id="validationDefault04" name="province" placeholder="กรุณากรอกจังหวัด" required="" type="text">
-								</div>
-								<div class="col-md-6 mb-6">
-									<label for="validationDefault05">รหัสไปรษณีย์</label>
-									<input class="form-control" id="validationDefault05" name="zipcode" placeholder="กรุณากรอกรหัสไปรษณีย์" required="" type="text">
-								</div>
+							<div class="form-group col-md-6">
+								<label for="validationDefault01">นามสกุล</label> <input class="form-control" id="validationDefault01" name="lastname" placeholder="กรุณากรอกนามสกุล" required="" type="text">
 							</div>
-						</div><button class="btn btn-primary" type="submit">Submit form</button>
+						</div>
+						<div class="form-row">
+							<div class="form-group col-md-6">
+								<label for="validationDefault01">Email</label> <input class="form-control" id="validationDefault01" placeholder="กรุณากรอก E-mail" required="" type="email">
+							</div>
+							<div class="form-group col-md-6">
+								<label for="validationDefault02">เบอร์โทรศัพท์ (Username)</label> <input class="form-control" id="validationDefault02" name="tel" placeholder="xxx-xxxxxxx" required="" type="text">
+							</div>
+						</div>
+						<div class="form-row">
+							<div class="form-group col-md-6" id="autoprovince">
+								<label for="validationDefault04">ตำบล</label> <input class="form-control" id="validationDefault04" name="district" placeholder="กรุณากรอกตำบล" required="" type="text">
+							</div>
+							<div class="form-group col-md-6" id="autoprovince">
+								<label for="validationDefault04">อำเภอ</label> <input class="form-control" id="validationDefault04" name="amphoe" placeholder="กรุณากรอกอำเภอ" required="" type="text">
+							</div>
+						</div>
+						<div class="form-row">
+							<div class="form-group col-md-6" id="autoprovince">
+								<label for="validationDefault04">จังหวัด</label> <input class="form-control" id="validationDefault04" name="province" placeholder="กรุณากรอกจังหวัด" required="" type="text">
+							</div>
+							<div class="form-group col-md-6" id="autoprovince">
+								<label for="validationDefault05">รหัสไปรษณีย์</label> <input class="form-control" id="validationDefault05" name="zipcode" placeholder="กรุณากรอกรหัสไปรษณีย์" required="" type="text">
+							</div>
+						</div>
+						<div class="text-right">
+							<button class="btn btn-outline-success" type="submit">Submit form</button>
+						</div>
 					</form>
 				</div>
 			</div>
@@ -138,79 +116,68 @@ include 'navbar.php';
 				</div>
 				<div class="card-body">
 					<form action="./include/getvariable.php" class="was-validated" id="inputpersonal" method="post" name="inputpersonal">
-						<div class="form-group">
-							<div class="row">
-								<div class="col-md-4 mb-4">
-									<p>โรคประจำตัว</p>
-									<label class="custom-control custom-radio">
-										<input class="custom-control-input" id="congenital_dis" name="congenital_dis" required="" type="radio" value="0">
-										<span class="custom-control-indicator"></span>
-										<span class="custom-control-description">ไม่มี</span>
-									</label> <label class="custom-control custom-radio">
-										<input class="custom-control-input" id="congenital_dis" name="congenital_dis" required="" type="radio" value="1">
-										<span class="custom-control-indicator"></span>
-										<span class="custom-control-description">มี</span>
-									</label>
-								</div>
-								<div class="col-md-4 mb-4">
-									<label>โรคประจำตัว</label>
-									<select class="custom-select d-block col" id="name_congenital_dis" name="name_congenital_dis" required="">
-										<!-- <select class="form-group custom-select d-block col" required> -->
-										<option disabled hidden="" selected value="">
-											-- โปรดเลือกโรคประจำตัว --
-										</option>
-										<option value="1">
-											โรคหัวใจ
-										</option>
-										<option value="2">
-											เบาหวาน
-										</option>
-										<option value="3">
-											ความดันโลหิต
-										</option>
-										<option value="4">
-											ข้อเข่าเสื่อม
-										</option>
-										<option value="5">
-											จอประสาทตาเสื่อม
-										</option>
-										<option value="6">
-											โรคอื่นๆ
-										</option>
-									</select>
-								</div>
-								<div class="col-md-4 mb-4">
-									<label>การเคลื่อนไหวร่างกาย</label>
-									<select class="custom-select d-block col" id="body_movement" name="body_movement" required="">
-										<option disabled hidden="" selected value="">
-											-- โปรดเลือกการเคลื่อนไหวร่างกาย --
-										</option>
-										<option value="1">
-											เดินได้ปกติ
-										</option>
-										<option value="2">
-											เดินได้เล็กน้อย
-										</option>
-										<option value="3">
-											เดินได้เล็กน้อย/ใช้ไม้เท้า
-										</option>
-										<option value="4">
-											เดินได้เล็กน้อย/ใช้รถเข็น
-										</option>
-										<option value="5">
-											เดินไม่ได้/ใช้รถเข็น
-										</option>
-									</select>
-								</div>
+						<div class="form-row">
+							<div class="form-group col-md-4">
+								<p>โรคประจำตัว</p><label class="custom-control custom-radio"><input class="custom-control-input" id="congenital_dis" name="congenital_dis" required="" type="radio" value="0"> <span class="custom-control-indicator"></span> <span class="custom-control-description">ไม่มี</span></label> <label class="custom-control custom-radio"><input class="custom-control-input" id="congenital_dis" name="congenital_dis" required="" type="radio" value="1"> <span class="custom-control-indicator"></span> <span class="custom-control-description">มี</span></label>
 							</div>
-						</div><button class="btn btn-success" type="submit">Submit form</button>
+							<div class="form-group col-md-4">
+								<label>โรคประจำตัว</label> <select class="custom-select d-block col" id="name_congenital_dis" name="name_congenital_dis" required="">
+									<!-- <select class="form-group custom-select d-block col" required> -->
+									<option disabled hidden="" selected value="">
+										-- โปรดเลือกโรคประจำตัว --
+									</option>
+									<option value="โรคหัวใจ">
+										โรคหัวใจ
+									</option>
+									<option value="เบาหวาน">
+										เบาหวาน
+									</option>
+									<option value="ความดันโลหิต">
+										ความดันโลหิต
+									</option>
+									<option value="ข้อเข่าเสื่อม">
+										ข้อเข่าเสื่อม
+									</option>
+									<option value="จอประสาทตาเสื่อม">
+										จอประสาทตาเสื่อม
+									</option>
+									<option value="โรคอื่นๆ">
+										โรคอื่นๆ
+									</option>
+								</select>
+							</div>
+							<div class="form-group col-md-4">
+								<label>การเคลื่อนไหวร่างกาย</label> <select class="custom-select d-block col" id="body_movement" name="body_movement" required="">
+									<option disabled hidden="" selected value="">
+										-- โปรดเลือกการเคลื่อนไหวร่างกาย --
+									</option>
+									<option value="เดินได้ปกติ">
+										เดินได้ปกติ
+									</option>
+									<option value="เดินได้เล็กน้อย">
+										เดินได้เล็กน้อย
+									</option>
+									<option value="เดินได้เล็กน้อย/ใช้ไม้เท้า">
+										เดินได้เล็กน้อย/ใช้ไม้เท้า
+									</option>
+									<option value="เดินได้เล็กน้อย/ใช้รถเข็น">
+										เดินได้เล็กน้อย/ใช้รถเข็น
+									</option>
+									<option value="เดินไม่ได้/ใช้รถเข็น">
+										เดินไม่ได้/ใช้รถเข็น
+									</option>
+								</select>
+							</div>
+						</div>
+						<div class="text-right">
+							<button class="btn btn-outline-success" type="submit">Submit form</button>
+						</div>
 					</form>
 				</div>
 			</div>
 		</div>
 		<hr>
 	</section>
-
 	<script src="./jquery.Thailand.js/dependencies/zip.js/zip.js" type="text/javascript">
 	</script> <!-- / dependencies for zip mode -->
 
@@ -221,45 +188,45 @@ include 'navbar.php';
 	<script src="./jquery.Thailand.js/dist/jquery.Thailand.min.js" type="text/javascript">
 	</script>
 	<script type="text/javascript">
-	                   /******************\
-	                    *     DEMO 1     *
-	                   \******************/
-	                   // demo 1: load database from json. if your server is support gzip. we recommended to use this rather than zip.
-	                   // for more info check README.md
-
-	                   $.Thailand({
-	                       database: './jquery.Thailand.js/database/db.json',
-
-	                       $district: $('#autoprovince [name="district"]'),
-	                       $amphoe: $('#autoprovince [name="amphoe"]'),
-	                       $province: $('#autoprovince [name="province"]'),
-	                       $zipcode: $('#autoprovince [name="zipcode"]'),
-
-	                       onDataFill: function(data){
-	                           console.info('Data Filled', data);
-	                       },
-
-	                       onLoad: function(){
-	                           console.info('Autocomplete is ready!');
-	                           $('#loader, .demo').toggle();
-	                       }
-	                   });
-
-	                   // watch on change
-
-	                   $('#autoprovince [name="district"]').change(function(){
-	                       console.log('ตำบล', this.value);
-	                   });
-	                   $('#autoprovince [name="amphoe"]').change(function(){
-	                       console.log('อำเภอ', this.value);
-	                   });
-	                   $('#autoprovince [name="province"]').change(function(){
-	                       console.log('จังหวัด', this.value);
-	                   });
-	                   $('#autoprovince [name="zipcode"]').change(function(){
-	                       console.log('รหัสไปรษณีย์', this.value);
-	                   });
-
+/******************\
+		 *     DEMO 1     *
+		\******************/
+		// demo 1: load database from json. if your server is support gzip. we recommended to use this rather than zip.
+		// for more info check README.md
+		$.Thailand(
+		{
+			database: './jquery.Thailand.js/database/db.json',
+			$district: $('#autoprovince [name="district"]'),
+			$amphoe: $('#autoprovince [name="amphoe"]'),
+			$province: $('#autoprovince [name="province"]'),
+			$zipcode: $('#autoprovince [name="zipcode"]'),
+			onDataFill: function(data)
+			{
+				console.info('Data Filled', data);
+			},
+			onLoad: function()
+			{
+				console.info('Autocomplete is ready!');
+				$('#loader, .demo').toggle();
+			}
+		});
+	// watch on change
+	$('#autoprovince [name="district"]').change(function()
+	{
+		console.log('ตำบล', this.value);
+	});
+	$('#autoprovince [name="amphoe"]').change(function()
+	{
+		console.log('อำเภอ', this.value);
+	});
+	$('#autoprovince [name="province"]').change(function()
+	{
+		console.log('จังหวัด', this.value);
+	});
+	$('#autoprovince [name="zipcode"]').change(function()
+	{
+		console.log('รหัสไปรษณีย์', this.value);
+	});
 	</script>
 </body>
 </html>
--- form_input_location.php
+++ form_input_location.php
@@ -1,119 +1,138 @@
-<div class="container">
-<h1>กรุณากรอกข้อมูลการท่องเที่ยว</h1>
-<div class="row">
-	<div class="col">
-		<div class="card">
-			<div class="card-header">
-				ข้อมูลการท่องเทื่ยว
-			</div>
-			<div class="card-body ">
-				<form id="#" class="was-validated" action="./include/getvariable.php" method="POST">
-					<div class="form-group">
-						<p>จังหวัดที่ต้องการท่องเที่ยว</p>
-						<label class="custom-control custom-checkbox">
-							<input type="checkbox" class="custom-control-input" value="1" name="check_province" required>
-							<span class="custom-control-indicator"></span>
-							<span class="custom-control-description">พิษณุโลก</span>
-						</label>
-						<label class="custom-control custom-checkbox">
-							<input type="checkbox" class="custom-control-input" value="2" name="check_province" required>
-							<span class="custom-control-indicator"></span>
-							<span class="custom-control-description">เพชรบูรณ์</span>
-						</label>
-						<label class="custom-control custom-checkbox">
-							<input type="checkbox" class="custom-control-input" value="3" name="check_province" required>
-							<span class="custom-control-indicator"></span>
-							<span class="custom-control-description">เชียงใหม่</span>
-						</label>
-						<label class="custom-control custom-checkbox">
-							<input type="checkbox" class="custom-control-input" value="4" name="check_province" required>
-							<span class="custom-control-indicator"></span>
-							<span class="custom-control-description">เชียงราย</span>
-						</label>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="container">
+		<h1>กรุณากรอกข้อมูลการท่องเที่ยว</h1>
+		<div class="row">
+			<div class="col">
+				<div class="card">
+					<div class="card-header">
+						ข้อมูลการท่องเทื่ยว
 					</div>
-
-
-					<div class="form-group">
-						<p>ประเภทการท่องเที่ยว</p>
-						<label class="custom-control custom-checkbox">
-							<input type="checkbox" class="custom-control-input" value="1" name="check_location" required>
-							<span class="custom-control-indicator"></span>
-							<span class="custom-control-description">เชิงธรรมชาติ</span>
-						</label>
-						<label class="custom-control custom-checkbox">
-							<input type="checkbox" class="custom-control-input" value="2" name="check_location" required>
-							<span class="custom-control-indicator"></span>
-							<span class="custom-control-description">เชิงวัฒนธรรม</span>
-						</label>
-						<label class="custom-control custom-checkbox">
-							<input type="checkbox" class="custom-control-input" value="3" name="check_location" required>
-							<span class="custom-control-indicator"></span>
-							<span class="custom-control-description">ความสนใจส่วนบุคคล</span>
-						</label>
+					<div class="card-body">
+						<form action="./include/getvariable.php" class="was-validated" id="#" method="post" name="#">
+							<div class="form-row">
+								<div class="form-group col-md-12">
+									<p>จังหวัดที่ต้องการท่องเที่ยว</p>
+									<label class="custom-control custom-radio">
+										<input class="custom-control-input" id="congenital_dis" name="congenital_dis" required="" type="radio" value="พิษณุโลก">
+										<span class="custom-control-indicator"></span>
+										<span class="custom-control-description">พิษณุโลก</span>
+									</label> <label class="custom-control custom-radio">
+										<input class="custom-control-input" id="congenital_dis" name="congenital_dis" required="" type="radio" value="เพชรบูรณ์">
+										<span class="custom-control-indicator"></span>
+										<span class="custom-control-description">เพชรบูรณ์</span>
+									</label> <label class="custom-control custom-radio">
+										<input class="custom-control-input" id="congenital_dis" name="congenital_dis" required="" type="radio" value="เชียงใหม่">
+										<span class="custom-control-indicator"></span>
+										<span class="custom-control-description">เชียงใหม่</span>
+									</label> <label class="custom-control custom-radio">
+										<input class="custom-control-input" id="congenital_dis" name="congenital_dis" required="" type="radio" value="เชียงราย">
+										<span class="custom-control-indicator"></span>
+										<span class="custom-control-description">เชียงราย</span>
+									</label>
+								</div>
+							</div>
+							<div class="form-row">
+								<div class="form-group col-md-6">
+									<p>ยานพาหนะในการเดินทาง</p><select class="custom-select d-block col" id="car" name="car" required="">
+										<option disabled hidden="" selected value="">
+											-- โปรดเลือก --
+										</option>
+										<option value="รถส่วนตัว">
+											รถส่วนตัว
+										</option>
+										<option value="รถประจำทาง">
+											รถประจำทาง
+										</option>
+										<option value="รถบัส">
+											รถบัส
+										</option>
+										<option value="รถตู้">
+											รถตู้
+										</option>
+										<option value="รถเช่า">
+											รถเช่า
+										</option>
+										<option value="อื่นๆ">
+											อื่นๆ
+										</option>
+									</select>
+								</div>
+								<div class="form-group col-md-6">
+									<p>ระยะเวลาที่ท่านท่องเที่ยว</p><select class="custom-select d-block col" id="traveltime" name="traveltime" required="">
+										<option disabled hidden="" selected value="">
+											-- โปรดเลือก --
+										</option>
+										<option value="ไปเช้าเย็นกลับ">
+											ไปเช้าเย็นกลับ
+										</option>
+										<option value="2 - 3 วัน">
+											2 - 3 วัน
+										</option>
+										<option value="4 – 5 วัน">
+											4 – 5 วัน
+										</option>
+										<option value="1 - 2 อาทิตย์">
+											1 - 2 อาทิตย์
+										</option>
+										<option value="มากกว่า 2 อาทิตย์">
+											มากกว่า 2 อาทิตย์
+										</option>
+									</select>
+								</div>
+							</div>
+							<div class="form-row">
+								<div class="form-group col-md-6">
+									<p>ที่พัก</p><select class="custom-select d-block col" id="camp" name="camp" required="">
+										<option disabled hidden="" selected value="">
+											-- โปรดเลือก --
+										</option>
+										<option value="บ้านตนเอง">
+											บ้านตนเอง
+										</option>
+										<option value="บ้านญาติ">
+											บ้านญาติ
+										</option>
+										<option value="โรงแรม">
+											โรงแรม
+										</option>
+										<option value="รีสอร์ท">
+											รีสอร์ท
+										</option>
+									</select>
+								</div>
+								<div class="form-group col-md-6">
+									<p>งบประมาณ</p><select class="custom-select d-block col" id="money" name="money" required="">
+										<option disabled hidden="" selected value="">
+											-- โปรดเลือก --
+										</option>
+										<option value="น้อยกว่า 1,000 บาท">
+											น้อยกว่า 1,000 บาท
+										</option>
+										<option value="1,000 - 3,000 บาท">
+											1,000 - 3,000 บาท
+										</option>
+										<option value="3,000 - 5,000 บาท">
+											3,000 - 5,000 บาท
+										</option>
+										<option value="มากกว่า 5,000 บาท">
+											มากกว่า 5,000 บาท
+										</option>
+									</select>
+								</div>
+							</div>
+							<div class="text-right">
+								<button class="btn btn-outline-primary" type="submit">ค้นหาสถานที่</button>
+							</div>
+						</form>
 					</div>
-
-					<div class="form-group">
-						<p>ท่านต้องการท่องเที่ยวรูปแบบ</p>
-						<select class="custom-select d-block col" name="travel" id="travel" required>
-							<option value="" selected disabled hidden>-- รูปแบบการท่องเที่ยว --</option>
-							<option value="">ครอบครัว</option>
-							<option value="1">กลุ่มเพื่อน</option>
-							<option value="2">แพคเกจท่องเที่ยว </option>
-							<option value="3">อื่นๆ </option>
-						</select>
-					</div>
-
-					<div class="form-group">
-						<p>ยานพาหนะในการเดินทาง</p>
-						<select class="custom-select d-block col" name="car" id="car" required>
-							<option value="" selected disabled hidden>-- โปรดเลือก --</option>
-							<option value="1">รถส่วนตัว </option>
-							<option value="2">รถประจำทาง</option>
-							<option value="3">รถบัส</option>
-							<option value="4">รถตู้</option>
-							<option value="5">รถเช่า</option>
-							<option value="6">อื่นๆ</option>
-						</select>
-					</div>
-
-					<div class="form-group">
-						<p>ระยะเวลาที่ท่านท่องเที่ยว</p>
-						<select class="custom-select d-block col" name="traveltime" id="traveltime" required>
-							<option value="" selected disabled hidden>-- โปรดเลือก --</option>
-							<option value="1">ไปเช้าเย็นกลับ </option>
-							<option value="2">2 - 3 วัน</option>
-							<option value="3">4 – 5 วัน</option>
-							<option value="4">1 - 2 อาทิตย์</option>
-							<option value="5">มากกว่า 2 อาทิตย์</option>
-						</select>
-					</div>
-
-					<div class="form-group">
-						<p>ที่พัก</p>
-						<select class="custom-select d-block col" name="camp" id="camp" required>
-							<option value="" selected disabled hidden>-- โปรดเลือก --</option>
-							<option value="1">บ้านตนเอง</option>
-							<option value="2">บ้านญาติ</option>
-							<option value="3">โรงแรม</option>
-							<option value="4">รีสอร์ท</option>
-						</select>
-					</div>
-					<div class="form-group">
-						<p>งบประมาณ</p>
-						<select class="custom-select d-block col" name="money" id="money" required>
-							<option value="" selected disabled hidden>-- โปรดเลือก --</option>
-							<option value="1">น้อยกว่า 1,000 บาท </option>
-							<option value="2">1,000 - 3,000 บาท</option>
-							<option value="3">3,000 - 5,000 บาท</option>
-							<option value="4">มากกว่า 5,000 บาท</option>
-						</select>
-					</div>
-					<div class="text-right">
-						<button type="button" class="btn btn-outline-primary">ค้นหาสถานที่</button>
-					</div>
-				</form>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-</div>
+</body>
+</html>
